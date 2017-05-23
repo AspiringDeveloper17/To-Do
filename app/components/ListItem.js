54357058c9ecfd3b3cdd153b35d8762927cf98d6
Shubh Sinha <sinha.shubhankar@gmail.com>
2017-05-23 09:41:09 -0500

    some changes

---
+++ app/components/ListItem.js
@@ -0,0 +1,54 @@
+import React, {
+  Component
+} from 'react';
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  Image,
+  StyleSheet
+} from 'react-native';
+
+export default class ListItem extends Component {
+  render() {
+    // We are going to return a simple list item with just a title for now
+    return (
+      <View style={styles.listItem}>
+        <Text style={styles.listItemTitle}>{this.props.task.name}</Text>
+        <TouchableHighlight
+          style={styles.hit}
+          onPress={this.props.onTaskCompletion}>
+          <Image style={styles.listItemAction}
+          source={require('./pics/sing.jpg')}
+             />
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  listItem: {
+    borderBottomColor: '#eee',
+    borderColor: 'gray',
+    flexDirection:'row',
+    alignItems:'center',
+    borderWidth: 1,
+    padding:20,
+  },
+  listItemTitle: {
+    flex: 6,
+    color: '#000',
+    fontSize: 16,
+
+  },
+  listItemAction: {
+    flex: 1,
+    width: 30,
+    height: 10,
+    marginTop:15
+  },
+  hit:{
+    backgroundColor:"transparent",
+  }
+});
